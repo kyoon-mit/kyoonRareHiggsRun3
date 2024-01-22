bab51fc3af8508b8746ae3fa7006d704429cfee7
Kyungseop Yoon <kyoon@mit.edu>
2024-01-22 11:29:54 -0500

    update to LHEP producer

--- fragments/fragment_ggh-hphigamma.py
+++ fragments/fragment_ggh-hphigamma.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 # Link to cards
-# https://github.com/cms-sw/genproductions/tree/master/bin/MadGraph5_aMCatNLO/cards/production/13p6TeV/Higgs/ggh012j_5f_NLO_FXFX_
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/MadGraph5_aMCatNLO/V2/ggh012j_5f_NLO_FXFX_125_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(10000),
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/Powheg/V2/gg_H_quark-mass-effects_slc7_amd64_gcc10_CMSSW_12_4_6_my_ggH_M125.tgz'),
+    nEvents = cms.untracked.uint32(500),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
     scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
@@ -13,7 +12,7 @@ externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import * ## Found here: https://github.com/cms-sw/cmssw/blob/f5cbbf1ab5806ffc54ccaab6a77201f35d63fca7/Configuration/Generator/python/MCTunesRun3ECM13p6TeV/PythiaCP5TuneUpSettings_cfi.py
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
-from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -25,7 +24,7 @@ generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
         pythia8CommonSettingsBlock,
         pythia8CP5SettingsBlock,
         pythia8PSweightsSettingsBlock,
-        pythia8aMCatNLOSettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
         processParameters = cms.vstring(
             'JetMatching:setMad = off',
             'JetMatching:scheme = 1',
@@ -40,17 +39,21 @@ generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
             'JetMatching:nQmatch = 5', # 4 corresponds to 4-flavour scheme (no matching of b-quarks), 5 for 5-flavour scheme
             'JetMatching:nJetMax = 2', # number of partons in born matrix element for highest multiplicity
             'SLHA:useDecayTable = off',
+            'POWHEG:nFinal = 1',   ## Number of final state particles
+                                   ## (BEFORE THE DECAYS) in the LHE
+                                   ## other than emitted extra parton
             '25:m0 = 125.0', ## higgs mass
             '25:onMode = off', ## switch off all decay channels
             '25:addChannel = 1 1.00 103 22 333' ## 22=photon, 333=phi(1020) \\ not sure what the others are
             '25:onIfMatch = 321 -321' ## K+ K-
         ),
     parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CP5Settings',
-                                    'pythia8PSweightsSettings',
-                                    'pythia8aMCatNLOSettings',
-                                    'processParameters',
-                                    )
+                                'pythia8CP5Settings',
+                                'pythia8PSweightsSettings',
+                                'pythia8aMCatNLOSettings',
+                                'pythia8PowhegEmissionVetoSettings',
+                                'processParameters',
+                                )
     )
 )
 
