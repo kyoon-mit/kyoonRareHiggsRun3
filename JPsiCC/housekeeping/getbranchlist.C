7b932068e50ca9697cf89d233a550e04febdc45c
Kyungseop Yoon <kyoon@mit.edu>
2024-03-27 17:02:39 -0400

    ROOT macro to get branch list

---
+++ JPsiCC/housekeeping/getbranchlist.C
@@ -0,0 +1,13 @@
+/*
+Housekeeping script for getting a list of branches from a ROOT file.
+Example usage:
+    root -l -b -q 'getbranchlist.C("<fileName>", "<treeName>")' > branchlist.txt
+*/
+
+void getbranchlist(const char* fileName, const char* treeName)
+{
+    TFile f(fileName);
+    TTree* tree = (TTree*) f.Get(treeName);
+    TObjArray* branchlist = tree->GetListOfBranches();
+    branchlist->Print();
+}
