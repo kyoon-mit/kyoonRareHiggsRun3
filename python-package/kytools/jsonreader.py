338918127fdf565a478fd35ce127bc1dd368f79d
Kyungseop Yoon <kyoon@mit.edu>
2024-06-12 16:58:40 -0400

    option to get rdf from spec file

--- python-package/kytools/jsonreader.py
+++ python-package/kytools/jsonreader.py
@@ -1,6 +1,6 @@
 import json
 import os
-from ROOT import TChain
+from ROOT import TChain, RDF
 
 def get_chain_from_json(anpath, jsonname, keys, treename='events'):
     """Opens ROOT files from a JSON file and returns a TChain.
@@ -33,7 +33,6 @@ def get_chain_from_json(anpath, jsonname, keys, treename='events'):
         chain.Add(item)
     return chain
 
-
 def get_chain_from_json_xrtd(anpath, jsonname, keys, treename='events', xrtd_proxy='root://xrootd.cmsaf.mit.edu//',):
     """Opens ROOT files from a JSON file and returns a TChain.
 
@@ -46,8 +45,8 @@ def get_chain_from_json_xrtd(anpath, jsonname, keys, treename='events', xrtd_pro
         keys (list(str)): List of the keys to the JSON object, in sequential order.
         treename (str): 
             Defaults to 'events'.
-        xrtd_proxy (str):
-            Name of the XRootD proxy. Defaults to root://xrootd.cmsaf.mit.edu'.
+        xrtd_proxy (str): Name of the XRootD proxy.
+            Defaults to root://xrootd.cmsaf.mit.edu'.
 
     Returns:
         chain (ROOT.TChain): TChain of the ROOT files.
@@ -70,3 +69,44 @@ def get_chain_from_json_xrtd(anpath, jsonname, keys, treename='events', xrtd_pro
         print('    ' + os.path.join(xrtd_proxy, *item.split('/')))
         chain.Add(os.path.join(xrtd_proxy, *item.split('/')))
     return chain
+
+def get_object_from_json(anpath, jsonname, keys):
+    """Retrieve an object from a JSON file.
+
+    Args:
+        anpath (str): Path to the analysis folder, relative to HRARE_DIR.
+            e.g. 'JPsiCC'
+        jsonname (str): Name of the JSON file.
+        keys (list(str)): List of the keys to the JSON object, in sequential order.
+
+    Returns:
+        jsonobject (list or dict): Retrieved object.
+
+    Raises:
+        TypeError: If keys object is not list(str).
+        TypeError: If json object is not list(str).
+    """
+    jsonfile = json.load(open(os.path.join(os.environ['HRARE_DIR'], anpath, 'json', jsonname)))
+    jsonobject = jsonfile
+    for i in range(len(keys)):
+        jsonobject = jsonobject[keys[i]]
+    if not all(isinstance(item, str) for item in keys):
+        raise TypeError('Argument provided in keys is not list(str).')
+    if not all(isinstance(item, str) for item in jsonobject):
+        raise TypeError('JSON object is not list(str).')
+    return jsonobject
+
+def get_rdf_from_json_spec(anpath, jsonname):
+    """Retrieve an RDataFrame from JSON spec file.
+
+    Args:
+        anpath (str): Path to the analysis folder, relative to HRARE_DIR.
+            e.g. 'JPsiCC'
+        jsonname (str): Name of the JSON file.
+
+    Returns:
+        rdf (ROOT.RDataFrame): Retrieved ROOT.RDataFrame object.
+    """
+    jsonfile = os.path.join(os.environ['HRARE_DIR'], anpath, 'json', jsonname)
+    rdf = RDF.Experimental.FromSpec(jsonfile)
+    return rdf
