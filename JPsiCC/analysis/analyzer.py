191deaa3696459025aac258656212155dc371f05
Kyungseop Yoon <kyoon@mit.edu>
2024-06-27 00:44:28 -0400

    checkpoint

--- JPsiCC/analysis/analyzer.py
+++ JPsiCC/analysis/analyzer.py
@@ -19,7 +19,7 @@ class JPsiCCAnalyzer:
 
     Args:
         SAMP (str): Either one of the following three options.
-            'DATA_BKG', 'MC_BKG', 'MC_SIG'
+            'DATA_BKG', 'MC_BKG1','MC_BKG2', 'MC_BKG3', 'MC_BKG4', 'MC_SIG'
         YEAR (int): Year of data-taking. Provide any number if using MC.
         VERS (str): Version of the files.
         CAT (str): Category of the analysis.
@@ -33,7 +33,10 @@ class JPsiCCAnalyzer:
     def __init__(self, SAMP, YEAR, VERS, CAT):
         match SAMP:
             case 'DATA_BKG': self._DATA, self._MODE = True, 'BKG'
-            case 'MC_BKG': self._DATA, self._MODE = False, 'BKG'
+            case 'MC_BKG1': self._DATA, self._MODE = False, 'BKG'
+            case 'MC_BKG2': self._DATA, self._MODE = False, 'BKG'
+            case 'MC_BKG3': self._DATA, self._MODE = False, 'BKG'
+            case 'MC_BKG4': self._DATA, self._MODE = False, 'BKG'
             case 'MC_SIG': self._DATA, self._MODE = False, 'SIG'
             case _: raise ValueError(f'SAMP={SAMP} is not a valid option.')
         if not type(YEAR) is int: raise TypeError(f'YEAR must be an integer.')
@@ -55,12 +58,13 @@ class JPsiCCAnalyzer:
         # Color scheme: http://arxiv.org/pdf/2107.02270
         self._orange = ROOT.kOrange + 1
         self._blue = ROOT.kAzure - 4
+        self._trueblue = ROOT.kBlue
         self._red = ROOT.kRed - 4
         self._purple = ROOT.kMagenta - 5
         self._gray = ROOT.kGray + 1
         self._violet = ROOT.kViolet + 2
 
-        self._draw_option = 'P' if self._DATA else 'HIST'
+        self._draw_option = 'P SAME' if self._DATA else 'HIST'
     
     def __draw_hist(self, histo1d, model1d, draw_option='HIST'):
         '''Internal method for drawing histogram.
@@ -90,7 +94,7 @@ class JPsiCCAnalyzer:
         Args:
             histo1d (ROOT.TH1D): The histogram.
             SAMP (str):  Either one of the following three options.
-                'DATA_BKG', 'MC_BKG', 'MC_SIG'.
+                'DATA_BKG', 'MC_BKG1', 'MC_BKG2', 'MC_BKG3', MC_BKG4', 'MC_SIG'.
 
         Returns:
             histo1d (ROOT.TH1D): The histogram.
@@ -99,27 +103,43 @@ class JPsiCCAnalyzer:
             case 'DATA_BKG':
                 histo1d.SetMarkerStyle(ROOT.kFullSquare)
                 histo1d.SetMarkerSize(0.5)
-            case 'MC_BKG':
+            case 'MC_BKG1':
+                histo1d.SetFillColorAlpha(self._gray, 0.6)
+                histo1d.SetLineColorAlpha(ROOT.kBlack, 1.0)
+            case 'MC_BKG2':
                 histo1d.SetFillColorAlpha(self._orange, 0.6)
+                histo1d.SetLineColorAlpha(ROOT.kBlack, 1.0)
+            case 'MC_BKG3':
+                histo1d.SetFillColorAlpha(self._red, 0.6)
+                histo1d.SetLineColorAlpha(ROOT.kBlack, 1.0)
+            case 'MC_BKG4':
+                histo1d.SetFillColorAlpha(self._violet, 0.6)
                 histo1d.SetLineColorAlpha(ROOT.kBlack, 1.0)
             case 'MC_SIG':
                 histo1d.SetFillColorAlpha(self._blue, 0.6)
                 histo1d.SetLineColorAlpha(ROOT.kBlack, 1.0)
         return histo1d
 
-    def __plot_hists(self, hist_dict, scaleSIG=1.):
+    def __plot_hists(self, hist_dict, draw=True, scaleSIG=1.):
         '''Internal method to plot histograms.
 
         Args:
             hist_dict (dict): Dictionary of histogram definitions.
-            scaleSIG (float): Scale factor for the signal histogram.
+            draw (bool, optional): Whether to draw histogram.
+                Defaults to True.
+            scaleSIG (float, optional): Scale factor for the signal histogram.
+                Defaults to 1.
 
         Returns:
             (None)
         '''
+        # counter = 0
         for key, hdef in hist_dict.items():
+            # counter += 1
+            # if counter > 1:
+            #     break
             # Create Histo1D
-            hname = f'{hdef["name"]}_{self._SAMPLE}_{self._YEAR}_{self._CAT}'
+            hname = f'{hdef["name"]}_{self._SAMPLE}_{self._YEAR}_{self._CAT}_{self._VERSION}'
             model1d = (hname, hdef['title'], hdef['bin'], hdef['xmin'], hdef['xmax'])
             histo1d = self._rdf.Histo1D(model1d, hdef['name'], 'w')
             # Set color
@@ -129,9 +149,33 @@ class JPsiCCAnalyzer:
             histo1d.SetDirectory(0)
             self._hists[key] = histo1d
             self._models[key] = model1d
+            print(f'INFO: Made histogram {key} for {self._SAMPLE}.')
             # Draw histogram
-            c = self.__draw_hist(histo1d=histo1d, model1d=model1d, draw_option=self._draw_option)
-            c.SaveAs(os.path.join(self._plotsavedir, f'{hname}.png'))
+            if draw:
+                c = self.__draw_hist(histo1d=histo1d, model1d=model1d, draw_option=self._draw_option)
+                c.SaveAs(os.path.join(self._plotsavedir, f'{hname}.png'))
+        return
+    
+    def createDataRDF(self, event_spec_json):
+
+        '''Open a sample using JSON spec and load it onto a RDF.
+
+        Args:
+            event_spec_json (str): Name of the JSON file for the "Events" tree.
+            data (bool, optional): Whether to use data.
+
+        Returns:
+            (None)
+
+        Raises:
+            Exception: If attempted for not DATA.
+        '''
+        if not self._DATA:
+            raise Exception('Please try again for data.')
+        rdf_events = jsonreader.get_rdf_from_json_spec(self._anpath, event_spec_json)
+        rdf_events, br1 = rdfdefines.rdf_def_sample_meta(rdf_events)
+        self._rdf, br2 = rdfdefines.rdf_def_weights(rdf_events, sum_weights=1., data=self._DATA)
+        self._branches = self._branches + br1 + br2
         return
     
     def createWeightedRDF(self, run_spec_json, event_spec_json):
@@ -151,7 +195,7 @@ class JPsiCCAnalyzer:
         rdf_runs, _ = rdfdefines.rdf_def_sample_meta(rdf_runs)
         rdf_events, br1 = rdfdefines.rdf_def_sample_meta(rdf_events)
         if self._DATA: sum_weights = 1.
-        else: sum_weights = rdfdefines.compute_sum_weights(rdf_runs)[0]
+        else: sum_weights = rdfdefines.compute_sum_weights(rdf_runs)
         self._rdf, br2 = rdfdefines.rdf_def_weights(rdf_events, sum_weights, data=self._DATA)
         self._branches = self._branches + br1 + br2
         return
@@ -170,12 +214,26 @@ class JPsiCCAnalyzer:
         match self._CAT:
             case 'GF':
                 new_rdf, br1 = rdfdefines.rdf_filter_triggers(self._rdf, self._CAT, self._YEAR)
-                new_rdf, br2 = rdfdefines.rdf_def_goodjets(new_rdf, self._CAT, self._YEAR)
-                new_rdf, br3 = rdfdefines.rdf_def_jpsi(new_rdf)
-                self._branches = self._branches + br1 + br2 + br3
+                new_rdf, br2 = rdfdefines.rdf_def_jpsi(new_rdf)
+                new_rdf, br3 = rdfdefines.rdf_def_muons(new_rdf)
+                new_rdf, br4 = rdfdefines.rdf_def_jets(new_rdf, self._CAT, self._YEAR)
+                self._branches = self._branches + br1 + br2 + br3 + br4
             case _:
                 new_rdf = self._rdf
         self._rdf = new_rdf
+        return
+    
+    def selectSampleRDF(self, sample_name):
+        '''Select sample name in RDF.
+
+        Args:
+            rdf (ROOT.RDataFrame): Input ROOT.RDataFrame.
+            sample_name (str): Name of the sample.
+
+        Returns:
+            (None)
+        '''
+        self._rdf = (self._rdf.Filter(f'sample=="{sample_name}"'))
         return
 
     def retrieveRDF(self):
@@ -186,15 +244,20 @@ class JPsiCCAnalyzer:
         '''
         return self._rdf
 
-    def snapshotRDF(self):
+    def snapshotRDF(self, name=''):
         '''Create a snapshot of the RDF.
 
         Outputs a file whose name is printed in the terminal.
 
+        Args:
+            name (str, optional): Name of the snapshot to create.
+                Defaults to ''. If '', then the snapshot name will be determined automatically.
+
         Returns:
             (None)
         '''
-        fname = f'snapshot_{self._sfx}.root'
+        if name=='': fname = f'snapshot_{self._sfx}.root'
+        else: fname = name
         self._rdf.Snapshot('Events', fname, self._branches)
         print('{}INFO: a snapshot has been created. >> {} {}'.format('\033[1;33m', fname, '\033[0m'))
         return
@@ -212,24 +275,35 @@ class JPsiCCAnalyzer:
         self._rdf = ROOT.RDataFrame(treename, filename)
         return
 
-    def makeHistos(self):
+    def makeHistos(self, plot=True, draw=True, scaleSIG=1.):
         '''Make histograms from the internal RDF.
 
-        Outputs histograms in the 'plots' directory.
+        If 'plot' is True, outputs histograms in the 'plots' directory. Otherwise,
+        returns a dictionary containing histogram definitions.
+
+        Args:
+            plot (bool, optional): If not true, function returns hist_dict.
+                Defaults to True.
+            draw (bool, optional): Whether to draw histogram.
+                Defaults to True.
+            scaleSIG (float, optional): Scale factor for the signal histogram.
+                Defaults to 1.
 
         Returns:
-            (None)
+            (None) or hist_dict (dict)
         '''
         hist_defs = jsonreader.get_object_from_json(anpath='JPsiCC',
                                                     jsonname='rdf_hists.json',
                                                     keys=['NANOAOD_to_RDF'])
         if not os.path.exists(self._plotsavedir): os.makedirs(self._plotsavedir)
+        hist_dict = dict()
         match self._CAT:
             case 'GF':
-                self.__plot_hists(hist_defs['Jpsi'])
+                hist_dict.update(hist_defs['Jpsi'])
                 # self.__plot_hists(hist_defs['jet'])
             case _: pass
-        return
+        if plot: self.__plot_hists(hist_dict, draw=draw, scaleSIG=scaleSIG)
+        else: return hist_dict
     
     def retrieveHisto(self, key):
         '''Retrieve a histogram object using a key.
@@ -249,7 +323,7 @@ class JPsiCCAnalyzer:
         '''Stack and plot histograms.
 
         Args:
-            analyzer (JPsiCCAnalyzer): another analyzer object.
+            analyzer (JPsiCCAnalyzer): Another analyzer object.
 
         Returns:
             (None)
@@ -266,33 +340,117 @@ class JPsiCCAnalyzer:
                 hs.Add(other_hist.GetPtr(), analyzer._draw_option)
                 # Draw histograms
                 model = self._models[key]
-                c_hstack = self.__draw_hist(hs, model, draw_option='HIST NOSTACK')
+                c_hstack = self.__draw_hist(hs, model, draw_option='HIST NOSTACK') #TODO: fix model hname
                 # Save TCanvas
                 sfx = 'STACK_' + self._sfx.lstrip(f'{self._SAMPLE}_')
                 c_hstack.SaveAs(os.path.join(self._plotsavedir, f'{key}_{sfx}.png'))
         return
     
-# Draw
-# save_dir = os.path.join(os.environ['HRARE_DIR'], 'JPsiCC', 'plots', 'jetstudies', 'control')
-# for hist in my_hists:
-#     c = ROOT.TCanvas()
-#     hist.Draw()
-#     c.SaveAs(os.path.join(save_dir, f'{hist.GetTitle()}.png'))
-#     c.Close()
+    def stackMultiHistos(self, bkg_analyzers, sig_analyzer, spec_key=''):
+        '''Stack and plot mlutiple histograms.
 
+        Args:
+            bkg_analyzers ([JPsiCCAnalyzer]): List of bkg analyzers.
+            sig_analyzer (JPsiCCAnalyzer): Signal analyzer.
+            spec_key (str, optional): Specify the key for drawing. If '', it will
+                draw every histogram for the keys that are in the analyzers.
+                Defaults to ''.
+
+        Returns:
+            (None)
+        '''
+        ROOT.gStyle.SetOptStat(0)
+        hist_dict = self.makeHistos(plot=False, draw=False)
+        for an in bkg_analyzers:
+            an.makeHistos(plot=True, draw=True)
+        sig_analyzer.makeHistos(plot=True, draw=True)
+
+        for key, hdef in hist_dict.items():
+            if spec_key != '':
+                if not spec_key == key: continue
+            # Create THStack
+            hname = f'{hdef["name"]}_MULTI_STACK_{self._YEAR}_{self._CAT}'
+            model1d = (hname, hdef['title'], hdef['bin'], hdef['xmin'], hdef['xmax'])
+            hs = ROOT.THStack()
+            
+            mc_bkg_norm, mc_bkg_max = 0., 0.
+            for an in bkg_analyzers:
+                hist = an.retrieveHisto(key)
+                if hist is None: continue
+                hist = self.__set_hist_style(hist, an._SAMPLE)
+                if an._DATA==False:
+                    mc_bkg_norm += hist.Integral()
+                    mc_bkg_max += hist.GetMaximum()
+                    hs.Add(hist.GetPtr(), an._draw_option)
+                else:
+                    data_hist = hist
+
+            c_hstack = ROOT.TCanvas()
+            hs.Draw()
+
+            # Draw data
+            data_hist.Scale(mc_bkg_norm/data_hist.Integral())
+            data_hist.Draw('P SAME')
+
+            # Draw signal
+            sig_hist = sig_analyzer.retrieveHisto(key)
+            if sig_hist is None: continue
+            sig_hist.Scale(mc_bkg_norm/sig_hist.Integral()) # scale sig hist
+            sig_hist = self.__set_hist_style(sig_hist, sig_analyzer._SAMPLE)
+            sig_hist.Draw('HIST SAME')
+            
+            # Draw histograms
+            hist_max_vals = [mc_bkg_max, data_hist.GetMaximum(), sig_hist.GetMaximum()]
+            print(hist_max_vals)
+            hs.SetMaximum(round(max(hist_max_vals)*1.10))
+            bin_width = (model1d[4]-model1d[3])/model1d[2]
+            hs.SetTitle(key)
+            hs.GetXaxis().SetTitle(model1d[1])
+            hs.GetYaxis().SetTitle(f'Events / {bin_width:.3g}')
+            c_hstack.Update()
+            # try: c_hstack = self.__draw_hist(hs, model1d, draw_option='')
+            # except:
+            #     print(f'ERROR: stackMultiHistos failed to created histogram {key}.')
+            #     continue
+            # Save TCanvas
+            sfx = 'MULTI_STACK_' + self._sfx.lstrip(f'{self._SAMPLE}_')
+            c_hstack.SaveAs(os.path.join(self._plotsavedir, f'{key}_{sfx}.png'))
+        return
+    
 if __name__=='__main__':
-    mcbkg = JPsiCCAnalyzer('MC_BKG', 2018, '202405', 'GF')
-    mcbkg.createWeightedRDF('run_spec_mc_bkg_2018.json', 'event_spec_mc_bkg_2018.json')
-    mcbkg.defineColumnsRDF()
-    mcbkg.snapshotRDF()
-    # mcbkg.readSnapshot('snapshot_MC_BKG_2018_GF_202405.root')
-    # mcbkg.makeHistos()
+    print('Choose preset (1, 2): ', end='')
+    preset = input()
+    if preset=='1': print('hello!')
+    mcbkg1 = JPsiCCAnalyzer('MC_BKG1', 2018, '202405', 'GF')
+    # mcbkg1.createWeightedRDF('run_spec_mc_bkg_2018.json', 'event_spec_mc_bkg_2018.json')
+    # mcbkg1.defineColumnsRDF()
+    # mcbkg1.snapshotRDF()
+    mcbkg1.readSnapshot('snapshot_MC_BKG_2018_GF_202405.root')
+    mcbkg1.selectSampleRDF('BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
+    # # mcbkg1.makeHistos()
+
+    mcbkg2 = JPsiCCAnalyzer('MC_BKG2', 2018, '202405', 'GF')
+    mcbkg2.readSnapshot('snapshot_MC_BKG_2018_GF_202405.root')
+    mcbkg2.selectSampleRDF('Charmonium_JpsiToMuMu_Pt25_46_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
+    # # mcbkg2.makeHistos()
+
+    mcbkg3 = JPsiCCAnalyzer('MC_BKG3', 2018, '202405', 'GF')
+    mcbkg3.readSnapshot('snapshot_MC_BKG_2018_GF_202405.root')
+    mcbkg3.selectSampleRDF('JpsiToMuMu_JpsiPt8_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2+MINIAODSIM')
+    # mcbkg3.makeHistos()
 
     databkg = JPsiCCAnalyzer('DATA_BKG', 2018, '202405', 'GF')
-    databkg.createWeightedRDF('run_spec_data_bkg_2018.json', 'event_spec_data_bkg_2018.json')
-    databkg.defineColumnsRDF()
-    databkg.snapshotRDF()
-    # databkg.readSnapshot('snapshot_DATA_BKG_2018_GF_202405.root')
+    # databkg.createDataRDF('event_spec_data_bkg_skim.json')
+    # databkg.defineColumnsRDF()
+    # databkg.snapshotRDF()
+    databkg.readSnapshot('snapshot_DATA_BKG_2018_GF_202405.root')
     # databkg.makeHistos()
 
-    mcbkg.stackHistos(databkg)
+    mcsig = JPsiCCAnalyzer('MC_SIG', 2018, '202405', 'GF')
+    # mcsig.createWeightedRDF('run_spec_mc_sig.json', 'event_spec_mc_sig.json')
+    # mcsig.defineColumnsRDF()
+    # mcsig.snapshotRDF()
+    mcsig.readSnapshot('snapshot_MC_SIG_2018_GF_202405.root')
+    # mcbkg1.makeHistos()
+
+    mcbkg1.stackMultiHistos([mcbkg1, mcbkg2, mcbkg3, databkg], mcsig)
--- JPsiCC/analysis/rdfdefines.py
+++ JPsiCC/analysis/rdfdefines.py
@@ -46,6 +46,7 @@ def compute_sum_weights(rdf_runs):
     gen_event_count = rdf_runs.Sum('genEventCount').GetValue()
     if not gen_event_count == gen_event_sumw:
         print('WARNING in compute_sum_weights:\ngen_event_sumw not equal to gen_events_count')
+        print(f'gen_event_sumw: {gen_event_sumw}, gen_event_count: {gen_event_count}')
     return gen_event_sumw, gen_event_count
 
 def rdf_def_sample_meta(rdf_events):
@@ -80,18 +81,8 @@ def rdf_def_weights(rdf_events, sum_weights, data=False):
         new_rdf (ROOT.RDataFrame): Modified RDataFrame.
         branches (list(str)): List of TBranches pertinent to the definitions.
     '''
-    # def_sum_weights = '''double def_sum_weights(unsigned int slot, const ROOT::RDF::RSampleInfo &id) {\n'''
-    # counter = 0
-    # for key, value in dict_sum_weights.items():
-    #     if counter == 0:
-    #         def_sum_weights += f'    if (id.GetS("sample_category") == "{key}") {{return {value};}}\n'
-    #     else:
-    #         def_sum_weights += f'    else if (id.GetS("sample_category") == "{key}") {{return {value};}}\n'
-    #     counter += 1
-    # def_sum_weights += '    else {return 0.0;}\n}'
-    # ROOT.gInterpreter.Declare(def_sum_weights)
     if not data: weight_exp = 'xsec*genWeight*lumi/sum_weights'
-    else: weight_exp = '1.0' # 'xsec*lumi/sum_weights'
+    else: weight_exp = '1.0'
     new_rdf = (rdf_events.Define('sum_weights', f'{sum_weights}')
                          .Define('w', weight_exp)
     )
@@ -119,9 +110,65 @@ def rdf_filter_triggers(rdf, CAT, YEAR):
             trigger = '(HLT_Dimuon20_Jpsi_Barrel_Seagulls || HLT_Dimuon25_Jpsi)'
         case _:
             raise ValueError(f'Trigger does not exist for the combination of {CAT} and {YEAR}.')
-    new_rdf = (rdf.Define('trigger', trigger)
-                  .Filter('(trigger==1)'))
+    try: new_rdf = rdf.Define('trigger', trigger)
+    except: new_rdf = rdf
+    new_rdf = new_rdf.Filter('(trigger==1)')
     branches = ['trigger']
+    return new_rdf, branches
+
+def rdf_def_jpsi(rdf):
+    '''RDF definition for J/Psi.
+
+    Args:
+        rdf (ROOT.RDataFrame): The ROOT.RDataFrame object.
+
+    Returns:
+        new_rdf (ROOT.RDataFrame): Modified ROOT.RDataFrame.
+        branches (list(str)): List of TBranches pertinent to the definitions.
+    '''
+    new_rdf = (rdf.Filter('nJpsi>0', 'Event must contain at least one J/psi with the given purity criteria.')
+                  )
+    branches = get_rdf_branches('Jpsi')
+    return new_rdf, branches
+
+def rdf_def_muons(rdf):
+    '''Muons RDF definition.
+
+    Args:
+        rdf (ROOT.RDataFrame): The ROOT.RDataFrame object.
+
+    Returns:
+        new_rdf (ROOT.RDataFrame): Modified ROOT.RDataFrame.
+    '''
+    new_rdf = rdf
+    branches = get_rdf_branches('muon')
+    return new_rdf, branches
+
+def rdf_def_jets(rdf, CAT, YEAR):
+    '''Jets RDF definition.
+
+    Args:
+        rdf (ROOT.RDataFrame): The ROOT.RDataFrame object.
+        CAT (str): Analysis category.
+        YEAR (int): Year of data-taking.
+
+    Returns:
+        new_rdf (ROOT.RDataFrame): Modified ROOT.RDataFrame.
+        branches (list(str)): List of TBranches pertinent to the definitions.
+
+    Raises:
+        ValueError: If the combination of 'CAT' and 'YEAR' does not correspond
+            to an existing trigger.
+    '''
+    load_functions()
+    match f'{CAT}_{YEAR}':
+        case 'GF_2018':
+            goodjets = '(Jet_pt > 20 && abs(Jet_eta) < 2.5 && Jet_btagDeepCvL > -1)'
+        case _:
+            raise ValueError(f'Good-jets definition does not exist for the combination of {CAT} and {YEAR}.')
+    new_rdf = (rdf.Define('goodjets', goodjets))
+    branches = ['goodjets']
+    branches += get_rdf_branches('jet')
     return new_rdf, branches
 
 def rdf_def_goodjets(rdf, CAT, YEAR):
@@ -149,76 +196,3 @@ def rdf_def_goodjets(rdf, CAT, YEAR):
                 #   .Filter('goodjets==true'))
     branches = ['goodjets']
     return new_rdf, branches
-
-def rdf_def_jpsi(rdf):
-    '''RDF definition for J/Psi.
-
-    Args:
-        rdf (ROOT.RDataFrame): The ROOT.RDataFrame object.
-
-    Returns:
-        new_rdf (ROOT.RDataFrame): Modified ROOT.RDataFrame.
-        branches (list(str)): List of TBranches pertinent to the definitions.
-    '''
-    new_rdf = (rdf.Filter('nJpsi>0', 'Event must contain at least one J/psi with the given purity criteria.')
-                  )
-    branches = get_rdf_branches('Jpsi')
-    return new_rdf, branches
-
-def rdf_def_jets(rdf):
-    '''Jets RDF definition.
-
-    Args:
-        rdf (ROOT.RDataFrame): The ROOT.RDataFrame object.
-
-    Returns:
-        new_rdf (ROOT.RDataFrame): Modified ROOT.RDataFrame.
-    '''
-    load_functions()
-    TRIGGER = 'HLT_Dimuon20_Jpsi_Barrel_Seagulls or HLT_Dimuon25_Jpsi' # TODO: both data and MC (only 2018)
-    GOODJETS = '(Jet_pt>20 and abs(Jet_eta)<2.5 and Jet_btagDeepCvL>-1)'
-    new_rdf= (rdf.Filter('nJpsi>0', 'at least one Jpsi')
-                # .Define('triggerAna', f'{TRIGGER}') # TODO: bool -> filter
-                .Define('goodJets', f'{GOODJETS}')
-                .Define('nGoodJets', 'Sum(goodJets)*1.0f').Filter('Sum(goodJets)>1', 'two jets for cc')
-                .Define('mJJ', 'Minv(Jet_pt[goodJets], Jet_eta[goodJets], Jet_phi[goodJets], Jet_mass[goodJets])')
-                # .Define('massHiggs', 'Minv3massive(Jet_pt[goodJets], Jet_eta[goodJets], Jet_phi[goodJets], Jet_mass[goodJets], Jpsi_kin_pt, Jpsi_kin_eta, Jpsi_kin_phi, Jpsi_kin_mass)')
-                # .Define('ptHiggs', 'Ptinv3massive(Jet_pt[goodJets], Jet_eta[goodJets], Jet_phi[goodJets], Jet_mass[goodJets], Jpsi_kin_pt, Jpsi_kin_eta, Jpsi_kin_phi, Jpsi_kin_mass)')
-                .Define('jet1Pt', 'Jet_pt[goodJets][0]')
-                .Define('jet2Pt', 'Jet_pt[goodJets][1]')
-                .Define('jet1Eta', 'Jet_eta[goodJets][0]')
-                .Define('jet2Eta', 'Jet_eta[goodJets][1]')
-                .Define('jet1CvL', 'Jet_btagDeepCvL[goodJets][0]')
-                .Define('jet2CvL', 'Jet_btagDeepCvL[goodJets][1]')
-                .Define('jet1partonFlavour', 'abs(Jet_partonFlavour[goodJets][0])')
-                .Define('jet2partonFlavour', 'abs(Jet_partonFlavour[goodJets][1])')
-                .Define('index_CloseFar', 'jetCloseFar(Jet_pt[goodJets],Jet_eta[goodJets],Jet_phi[goodJets],Jet_mass[goodJets],Jpsi_kin_pt,Jpsi_kin_eta, Jpsi_kin_phi,Jpsi_kin_mass)')
-                .Filter('index_CloseFar[0]!= -1', 'at least one close jet')
-                .Define('jetCloseCvL', 'Jet_btagDeepCvL[goodJets][index_CloseFar[0]]')
-                .Define('jetFarCvL', 'Jet_btagDeepCvL[goodJets][index_CloseFar[1]]')
-                .Define('jetClosePt', 'Jet_pt[goodJets][index_CloseFar[0]]')
-                .Define('jetFarPt', 'Jet_pt[goodJets][index_CloseFar[1]]')
-                .Define('jetFarPtCharm', '(abs(Jet_partonFlavour[goodJets][index_CloseFar[1]])==4) ? Jet_cRegCorr[goodJets][index_CloseFar[1]] * Jet_pt[goodJets][index_CloseFar[1]]:-1.')
-                .Define('jetFarPtGluon', '(abs(Jet_partonFlavour[goodJets][index_CloseFar[1]])==21) ? Jet_cRegCorr[goodJets][index_CloseFar[1]] * Jet_pt[goodJets][index_CloseFar[1]]:-1.')
-                .Define('jetFarCvLCharm', '(abs(Jet_partonFlavour[goodJets][index_CloseFar[1]])==4) ? Jet_btagDeepCvL[goodJets][index_CloseFar[1]]:-1.')
-                .Define('jetFarCvLGluon', '(abs(Jet_partonFlavour[goodJets][index_CloseFar[1]])==21) ? Jet_btagDeepCvL[goodJets][index_CloseFar[1]]:-1.')
-                .Define('jetCloseScale', 'Jet_pt[goodJets][index_CloseFar[0]]/GenJet_pt[Jet_genJetIdx[goodJets][index_CloseFar[0]]]')
-                .Define('jetFarScale', 'Jet_pt[goodJets][index_CloseFar[1]]/GenJet_pt[Jet_genJetIdx[goodJets][index_CloseFar[1]]]')
-                .Define('jetClosecRegCorrScale', 'Jet_pt[goodJets][index_CloseFar[0]]*Jet_cRegCorr[goodJets][index_CloseFar[0]]/GenJet_pt[Jet_genJetIdx[goodJets][index_CloseFar[0]]]')
-                .Define('jetFarcRegCorrScale', 'Jet_pt[goodJets][index_CloseFar[1]]*Jet_cRegCorr[goodJets][index_CloseFar[1]]/GenJet_pt[Jet_genJetIdx[goodJets][index_CloseFar[1]]]')     
-                .Define('jetClosecRegCorr', 'Jet_cRegCorr[goodJets][index_CloseFar[0]]')
-                .Define('jetFarcRegCorr', 'Jet_cRegCorr[goodJets][index_CloseFar[1]]')
-                .Define('jetClosepartonFlavour', 'abs(Jet_partonFlavour[goodJets][index_CloseFar[0]])')
-                .Define('jetFarpartonFlavour', 'abs(Jet_partonFlavour[goodJets][index_CloseFar[1]])')
-                .Define('jetClosenConst', 'Jet_nConstituents[goodJets][index_CloseFar[0]]')
-                .Define('jetFarnConst', 'Jet_nConstituents[goodJets][index_CloseFar[1]]')
-                .Define('jetCloseJPsiRatio', 'Jpsi_kin_pt[0]/Jet_pt[goodJets][index_CloseFar[0]]')
-                .Define('jetClosenMuons', 'Jet_nMuons[goodJets][index_CloseFar[0]]')
-                .Define('jetFarnMuons', 'Jet_nMuons[goodJets][index_CloseFar[1]]')
-                .Define('jetClosenElectrons', 'Jet_nElectrons[goodJets][index_CloseFar[0]]')
-                .Define('jetFarnElectrons', 'Jet_nElectrons[goodJets][index_CloseFar[1]]')
-                .Define('massHiggsCorr', 'Minv3massiveCorr(Jet_pt[goodJets], Jet_eta[goodJets], Jet_phi[goodJets], Jet_mass[goodJets], Jet_cRegCorr[goodJets], Jet_muonIdx1[goodJets][index_CloseFar[0]], Jet_muonIdx2[goodJets][index_CloseFar[0]], Muon_pt, Muon_eta, Muon_phi, Jpsi_muon1_pt, Jpsi_muon1_eta, Jpsi_muon1_phi, Jpsi_muon2_pt, Jpsi_muon2_eta, Jpsi_muon2_phi, Jpsi_kin_pt, Jpsi_kin_eta, Jpsi_kin_phi, Jpsi_kin_mass, index_CloseFar[0])')
-                .Define('minDRjpsi', 'std::min(deltaR(Jet_eta[goodJets][0], Jet_phi[goodJets][0], Jpsi_kin_eta[0], Jpsi_kin_phi[0]),deltaR(Jet_eta[goodJets][1], Jet_phi[goodJets][1], Jpsi_kin_eta[0], Jpsi_kin_phi[0]))')
-                .Filter('minDRjpsi<0.3', 'jPsi very close to 1 charm-jet')
-            )
-    return new_rdf
