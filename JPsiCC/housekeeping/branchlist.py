0c98098f9d76758a7a8f9390c663c268b90c9333
Kyungseop Yoon <kyoon@mit.edu>
2024-08-07 10:07:07 -0400

    update

--- JPsiCC/housekeeping/branchlist.py
+++ JPsiCC/housekeeping/branchlist.py
@@ -20,12 +20,12 @@ def make_json(stuff):
     return json_str
 
 if __name__=='__main__':
-    mcbkg = 'root://xrootd.cmsaf.mit.edu//store/user/paus/nanohr/D04/BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM/00573B89-23D5-2347-8CF2-6B9DA7D2A469.root'
-    databkg = 'root://xrootd.cmsaf.mit.edu//store/user/paus/nanohr/D04/Charmonium+Run2018B-UL2018_MiniAODv2-v1+MINIAOD/00FD550B-8D26-E046-B901-9409005B09C5.root'
-    mcsig = '/data/submit/cms/store/user/mariadlf/nano/GluGluH_HJPsiCC/NANOAOD_test3/step7_GluGluH_HJPsiCC_1.root'
+    mcbkg = '/data/submit/kyoon/D04/BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM/00573B89-23D5-2347-8CF2-6B9DA7D2A469.root'
+    databkg = '/scratch/submit/cms/mariadlf/Hrare/newSKIMS/D04/BPH/2018/Charmonium+RunA/output_CharmoniumA_2018_0.root'
+    mcsig = '/data/submit/cms/store/user/mariadlf/nano/GluGluH_HJPsiCC/NANOAOD_D04/nano_GluGluH_HJPsiCC_1.root'
     my_json_dict = dict()
     my_json_dict['MC_BKG'] = {'Events': get_branch_name_description(mcbkg, 'Events'), 'Runs': get_branch_name_description(mcbkg, 'Runs')}
-    my_json_dict['DATA_BKG'] = {'Events': get_branch_name_description(databkg, 'Events'), 'Runs': get_branch_name_description(databkg, 'Runs')}
+    my_json_dict['DATA_BKG'] = {'Events': get_branch_name_description(databkg, 'Events')}
     my_json_dict['MC_SIG'] = {'Events': get_branch_name_description(mcsig, 'Events'), 'Runs': get_branch_name_description(mcsig, 'Runs')}
     json_str = make_json(my_json_dict)
     print(json_str)
