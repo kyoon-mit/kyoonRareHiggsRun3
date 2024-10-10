6ed96b4b5616b0592be0f747758c2dbe8247f0a7
Kyungseop Yoon <kyoon@mit.edu>
2024-10-10 08:41:50 -0400

    removing Custom directory

--- Custom/nanoaod_test/python/CustomPFWeights_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-abc = cms.EDProducer("CustomPFWeightProducer",
-                     candName = cms.InputTag("packedPFCandidates"),
-                     graph_path = cms.FileInPath("CommonTools/PileupAlgos/plugins/AttentionBasedPileupRejectionModel_Run2.pb"),
-                     preprocess_json = cms.FileInPath("CommonTools/PileupAlgos/plugins/preprocessing_info.json")
-)
