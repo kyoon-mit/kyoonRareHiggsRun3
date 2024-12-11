1ebd011fa96baf794f024ed8cb3f55e9c90ab2e3
Kyungseop Yoon <kyoon@mit.edu>
2024-12-11 14:29:24 -0500

    heatmap option

--- JPsiCC/studies/jet_flavor.py
+++ JPsiCC/studies/jet_flavor.py
@@ -1,10 +1,10 @@
 from os import path, environ, makedirs
 from kytools import rootio
-import json
 from matplotlib import pyplot as plt
+import numpy as np
 # TODO: Parse arguments
 
-def get_pandas_dataframes(sig_filename, bkg_filename, tree_name, branches, **kwargs):
+def get_pandas_dataframes(sig_filename, bkg_filename, tree_name, branches, weight_var='', **kwargs):
     """Converts the given TTree in ROOT files into pandas.DataFrames.
 
     This method opens the ROOT files, reads the values in the specified TTree, and converts them into
@@ -17,6 +17,9 @@ def get_pandas_dataframes(sig_filename, bkg_filename, tree_name, branches, **kwa
         tree_name (str): Name of the TTree in the ROOT file.
         branches (list(str)): The MVA input variables to include in the pandas.DataFrame.
             It is okay to pass variables that are not used in training.
+        weight_var (str, optional): If provided, the function will look for the
+            weight column of that name in the pandas.DataFrame.
+            Defaults to ''.
         **kwargs (optional): Other arguments that are passed but not used.
             This is to avoid getting an error in the case where **dict is passed and
             there are too many keys.
@@ -26,13 +29,17 @@ def get_pandas_dataframes(sig_filename, bkg_filename, tree_name, branches, **kwa
         bkg_df (pandas.DataFrame): DataFrame of the background dataset.
     """
     print('INFO: executing get_pandas_dataframes.')
+    if weight_var: branches += weight_var
     sig_df, bkg_df =\
         rootio.root_to_pandas(sig_filename, tree_name, branches),\
         rootio.root_to_pandas(bkg_filename, tree_name, branches)
     return sig_df, bkg_df
 
 
-def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatter=True, terminal=False, **kwargs):
+def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatter=True,
+                        heatmap=True, terminal=False, bin_range=[[-1., 1.], [-1., 1.]],
+                        weight_var='', sfx='',
+                        **kwargs):
     """Computes numerical and graphical correlations from the given pandas.DataFrames.
 
     Each and every variable in the pandas.DataFrame object is compared against
@@ -45,6 +52,8 @@ def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatte
     If `scatter` is True, scatter plots are created and saved. For each column
     that is common in both bkg_df and sig_df, three scatter plots are shown:
     signal, background, and combined (in different colors).
+
+    The option 'heatmap' is similar to 'scatter', except that a heatmap is shown.
 
     If `slices` is True, a histogram of the MVA discrimininator is created in binned ranges
     of each variable.
@@ -59,8 +68,20 @@ def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatte
             Defaults to True.
         scatter (bool, optional): Whether to plot the scatter plots.
             Defaults to True.
+        heatmap (bool, optional): Wheter to plot the heatmaps.
+            Defaults to True.
         terminal (bool, optional): Whether to show results in the terminal.
             Defaults to True.
+        bin_range ([[float, float], [float, float]], optional): Used only if
+            'heatmap' is True. Provides the boundaries of the outermost bins in
+            the format of [[xmin, xmax], [ymin, ymax]].
+            Defaults to [[-1., 1.], [-1., 1.]].
+        weight_var (str, optional): If provided, the function will look for the
+            weight column of that name in the pandas.DataFrame. Relevant only if
+            'heatmap' is True.
+            Defaults to ''.
+        sfx (str, optional): Suffix of the file name.
+            Defaults to ''.
         **kwargs (optional): Other arguments that are passed but not used.
             This is to avoid getting an error in the case where **dict is passed and
             there are too many keys.
@@ -75,7 +96,7 @@ def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatte
         print('INFO: calculating correlation matrix.')
         sig_corr = sig_df.corr(method='pearson').to_string()
         bkg_corr = bkg_df.corr(method='pearson').to_string()
-        text_file_name = path.join(save_path, f'CORRELATION_MATRIX.txt')
+        text_file_name = path.join(save_path, f'CORRELATION_MATRIX{'_' if sfx else ''}{sfx}.txt')
         text_input = [f'<Signal>\n{sig_corr}',
                       '\n',
                       f'<Background>\n{bkg_corr}']
@@ -92,13 +113,13 @@ def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatte
         for col in list(set(sig_columns).intersection(bkg_columns)):
             ax_sig = sig_df.plot.scatter(x=col, y=corr_var, c='Red', s=2.5, alpha=0.01)
             ax_sig.set_title(f'Signal: \n{corr_var} vs. {col}')
-            plt.savefig(path.join(save_path, f'SCATTER_PLOT_sig_{corr_var}_VS_{col}.png'),
-                        dpi=800, pad_inches=0.05)
+            plt.savefig(path.join(save_path, f'SCATTER_PLOT_sig_{corr_var}_VS_{col}{'_' if sfx else ''}{sfx}.png'),
+                        dpi=400, pad_inches=0.05)
             plt.close()
             ax_bkg = bkg_df.plot.scatter(x=col, y=corr_var, c='Blue', s=2.5, alpha=0.002)
             ax_bkg.set_title(f'Background: \n{corr_var} vs. {col}')
-            plt.savefig(path.join(save_path, f'SCATTER_PLOT_bkg_{corr_var}_VS_{col}.png'),
-                        dpi=800, pad_inches=0.05)
+            plt.savefig(path.join(save_path, f'SCATTER_PLOT_bkg_{corr_var}_VS_{col}{'_' if sfx else ''}{sfx}.png'),
+                        dpi=400, pad_inches=0.05)
             plt.close()
             ax_comb = plt.subplot(111)
             ax_comb.scatter(x=bkg_df[col], y=bkg_df[corr_var], c='Blue', s=2.5, alpha=0.002, label='background')
@@ -107,8 +128,48 @@ def compute_correlation(sig_df, bkg_df, corr_var, save_path, matrix=True, scatte
             ax_comb.set_xlabel(col)
             ax_comb.set_ylabel(corr_var)
             for lh in ax_comb.legend().legend_handles: lh.set_alpha(1)
-            plt.savefig(path.join(save_path, f'SCATTER_PLOT_comb_{corr_var}_VS_{col}.png'),
-                        dpi=800, pad_inches=0.05)
+            plt.savefig(path.join(save_path, f'SCATTER_PLOT_comb_{corr_var}_VS_{col}{'_' if sfx else ''}{sfx}.png'),
+                        dpi=400, pad_inches=0.05)
+            plt.close()
+    # Create heatmaps
+    if heatmap:
+        sig_columns, bkg_columns = list(sig_df), list(bkg_df)
+        sig_columns.remove(corr_var)
+        bkg_columns.remove(corr_var)
+        if weight_var:
+            sig_columns.remove(weight_var)
+            bkg_columns.remove(weight_var)
+        for col in list(set(sig_columns).intersection(bkg_columns)):
+            print('INFO: creating heatmap.')
+            weights = bkg_df[weight_var] if weight_var else None
+            heatmap, xedges, yedges = np.histogram2d(x=bkg_df[col], y=bkg_df[corr_var],
+                                                    bins=40, range=bin_range, weights=weights)
+            # heatmap = np.array([[1, 4], [2, 3]]) # for testing
+            # xedges, yedges = [0., 0.5, 1.], [0., 0.5, 1.]
+            extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+            plt.clf()
+            _, ax_bkg = plt.subplots()
+            im = plt.imshow(heatmap.T, extent=extent, cmap='viridis', origin='lower')
+            ax_bkg.set_title(f'Background: \n{corr_var} vs. {col}')
+            ax_bkg.set_xlabel(col)
+            ax_bkg.set_ylabel(corr_var)
+            plt.colorbar(im)
+            plt.savefig(path.join(save_path, f'HEATMAP_bkg_{corr_var}_VS_{col}{'_' if sfx else ''}{sfx}.png'),
+                        dpi=400, pad_inches=0.05)
+            plt.close()
+            plt.clf()
+            #
+            weights = sig_df[weight_var] if weight_var else None
+            heatmap, xedges, yedges = np.histogram2d(x=sig_df[col], y=sig_df[corr_var],
+                                                    bins=40, range=bin_range, weights=weights)
+            _, ax_sig = plt.subplots()
+            im = plt.imshow(heatmap.T, extent=extent, cmap='viridis', origin='lower')
+            ax_sig.set_title(f'Signal: \n{corr_var} vs. {col}')
+            ax_sig.set_xlabel(col)
+            ax_sig.set_ylabel(corr_var)
+            plt.colorbar(im)
+            plt.savefig(path.join(save_path, f'HEATMAP_sig_{corr_var}_VS_{col}{'_' if sfx else ''}{sfx}.png'),
+                        dpi=400, pad_inches=0.05)
             plt.close()
     return
     
@@ -228,7 +289,8 @@ if __name__=='__main__':
         'sig_filename': '/work/submit/kyoon/CMSSW_13_3_0/src/RareHiggsRun3/JPsiCC/analysis/snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_20241120_WEIGHT_no_filter.root',
         'bkg_filename': '/work/submit/kyoon/CMSSW_13_3_0/src/RareHiggsRun3/JPsiCC/analysis/snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241120_WEIGHT_no_filter.root',
         'tree_name': 'Events',
-        'branches': ['jetFar_CvL', 'jetFar_CvB'],
+        'branches': ['jetFar_CvL', 'jetFar_CvB', 'jetClose_CvL', 'jetClose_CvB'],
+        'weight_var': 'w',
         'corr_var': 'jetFar_CvL'
     }
 
@@ -236,4 +298,6 @@ if __name__=='__main__':
     params['sig_df'], params['bkg_df'] = get_pandas_dataframes(**params)
 
     ### Compute correlation
-    compute_correlation(terminal=False, **params)
+    compute_correlation(scatter=False, terminal=False, bin_range=[[0., 1.], [0., 1.]], sfx='no_filter_jet_pt_0', **params)
+    params['corr_var'] = 'jetClose_CvL'
+    compute_correlation(scatter=False, terminal=False, bin_range=[[0., 1.], [0., 1.]], sfx='no_filter_jet_pt_0', **params)
