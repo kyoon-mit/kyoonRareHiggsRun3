eba45058f79ee5f363a7f7898e7cc094c787ebf6
Kyungseop Yoon <kyoon@mit.edu>
2025-01-10 10:39:59 -0500

    20250110 sync test

--- JPsiCC/analysis/preset_rdf.py
+++ JPsiCC/analysis/preset_rdf.py
@@ -2,42 +2,92 @@ from jpsicc import analyzer as an
 from datetime import date
 
 if __name__=='__main__':
-    print('Choose preset (mcbkg, databkg, mcsig, allsnap, gen, allplots, noweight, cut, ancut): ', end='')
-    preset = input()
+    from sys import argv
+    print(argv)
+    print('Choose preset (full, stack, mcsig, mcbkg, data, allsnap, gen, allplots, noweight, cut): ', end='')
+    # preset = input()
+    preset = argv[1]
+    print(preset)
     today = date.today()
     today_format = f'{today.year}{today.month:02}{today.day:02}'
+    sfx = 'synctest'
     match preset:
-        case 'mcbkg':
+        case 'full':
+            mcsig = an.JPsiCCLoader('MC_SIG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
+            mcsig.createWeightedRDF('run_spec_mc_sig.json', 'event_spec_mc_sig.json')
+            mcsig.defineColumnsRDF(filter_trigger=True, filter_vertex=True, filter_muons=True,
+                                   filter_jpsi=True, filter_jets=True, filter_higgs=True)
+            mcsig.snapshotRDF(user_sfx=sfx)
+            
             mcbkg = an.JPsiCCLoader('MC_BKG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
             mcbkg.createWeightedRDF('run_spec_mc_bkg_2018.json', 'event_spec_mc_bkg_2018.json')
-            mcbkg.defineColumnsRDF(filter_trigger=False, filter_vertex=False, filter_muons=False,
-                                   filter_jpsi=False, filter_jets=False, filter_higgs=False)
-            mcbkg.snapshotRDF(user_sfx='no_filter')
-            mcbkg.readSnapshot(f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_no_filter.root')
-            mcbkg.makeHistos(genplots=False, cdf=True, save=True, draw=True, normSIG=False,
-                             user_sfx='no_filter')
+            mcbkg.defineColumnsRDF(filter_trigger=True, filter_vertex=True, filter_muons=True,
+                                   filter_jpsi=True, filter_jets=True, filter_higgs=True)
+            mcbkg.snapshotRDF(user_sfx=sfx)
 
-        case 'databkg':
-            databkg = an.JPsiCCLoader('DATA_BKG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
-            databkg.createDataRDF('event_spec_data_bkg_skim.json')
-            databkg.defineColumnsRDF(filter_trigger=False, filter_vertex=False, filter_muons=False,
-                                   filter_jpsi=False, filter_jets=False, filter_higgs=False)
-            databkg.snapshotRDF(user_sfx='no_filter')
-            databkg.readSnapshot(f'snapshot_DATA_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_no_filter.root')
-            databkg.makeHistos(genplots=False, cdf=True, save=True, draw=True, normSIG=False,
-                             user_sfx='no_filter')
+            data = an.JPsiCCLoader('DATA', 2018, '202410', 'GF', 'CMSSW_13_3_0')
+            data.createDataRDF('event_spec_data_skim.json')
+            data.defineColumnsRDF(filter_trigger=True, filter_vertex=True, filter_muons=True,
+                                    filter_jpsi=True, filter_jets=True, filter_higgs=True)
+            data.snapshotRDF(user_sfx=sfx)
+
+            analyzer = an.JPsiCCAnalyzer(2018, '202410', 'GF', 'CMSSW_13_3_0', weights=True)
+            analyzer.readSnapshotSAMP('MC_SIG', f'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            analyzer.readSnapshotSAMP('MC_BKG1', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root',
+                                        sample_name='BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
+            analyzer.readSnapshotSAMP('MC_BKG2', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root',
+                                        sample_name='JpsiToMuMu_JpsiPt8_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2+MINIAODSIM')
+            analyzer.readSnapshotSAMP('DATA', f'snapshot_DATA_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            # analyzer.addCutOptions('')
+            analyzer.stackMultiHistos(draw_indiv=False,
+                                      user_sfx=sfx)
+            
+        case 'stack':
+            analyzer = an.JPsiCCAnalyzer(2018, '202410', 'GF', 'CMSSW_13_3_0', weights=True)
+            analyzer.readSnapshotSAMP('MC_SIG', f'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            analyzer.readSnapshotSAMP('MC_BKG1', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root',
+                                        sample_name='BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
+            analyzer.readSnapshotSAMP('MC_BKG2', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root',
+                                        sample_name='JpsiToMuMu_JpsiPt8_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2+MINIAODSIM')
+            analyzer.readSnapshotSAMP('DATA', f'snapshot_DATA_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            analyzer.addCutOptions('')
+            analyzer.stackMultiHistos(keys=['jetPt',
+                                            'goodJetPt',
+                                            'jetClose_pt',
+                                            'jetFar_pt'],
+                                      user_sfx=sfx)
 
         case 'mcsig':
             # verbosity = ROOT.Experimental.RLogScopedVerbosity(ROOT.Detail.RDF.RDFLogChannel(), ROOT.Experimental.ELogLevel.kInfo)
             mcsig = an.JPsiCCLoader('MC_SIG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
             mcsig.createWeightedRDF('run_spec_mc_sig.json', 'event_spec_mc_sig.json')
-            mcsig.defineColumnsRDF(filter_trigger=False, filter_vertex=False, filter_muons=False,
-                                   filter_jpsi=False, filter_jets=False, filter_higgs=False)
-            mcsig.snapshotRDF(user_sfx='no_filter')
-            mcsig.readSnapshot(f'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_no_filter.root')
-            mcsig.makeHistos(genplots=False, cdf=True, save=True, draw=True, normSIG=False,
-                             user_sfx='no_filter')
-        
+            mcsig.defineColumnsRDF(filter_trigger=True, filter_vertex=True, filter_muons=True,
+                                   filter_jpsi=True, filter_jets=True, filter_higgs=True)
+            mcsig.snapshotRDF(user_sfx=sfx)
+            mcsig.readSnapshot(f'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            mcsig.makeHistos(genplots=False, cdf=False, save=True, draw=True, normSIG=False,
+                             user_sfx=sfx)
+            
+        case 'mcbkg':
+            mcbkg = an.JPsiCCLoader('MC_BKG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
+            mcbkg.createWeightedRDF('run_spec_mc_bkg_2018.json', 'event_spec_mc_bkg_2018.json')
+            mcbkg.defineColumnsRDF(filter_trigger=False, filter_vertex=False, filter_muons=False,
+                                   filter_jpsi=False, filter_jets=True, filter_higgs=False)
+            mcbkg.snapshotRDF(user_sfx=sfx)
+            mcbkg.readSnapshot(f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            mcbkg.makeHistos(genplots=False, cdf=False, save=True, draw=True, normSIG=False,
+                             user_sfx=sfx)
+            
+        case 'data':
+            databkg = an.JPsiCCLoader('DATA', 2018, '202410', 'GF', 'CMSSW_13_3_0')
+            databkg.createDataRDF('event_spec_data_skim.json')
+            databkg.defineColumnsRDF(filter_trigger=False, filter_vertex=False, filter_muons=False,
+                                   filter_jpsi=False, filter_jets=True, filter_higgs=False)
+            databkg.snapshotRDF(user_sfx=sfx)
+            databkg.readSnapshot(f'snapshot_DATA_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root')
+            databkg.makeHistos(genplots=False, cdf=False, save=True, draw=True, normSIG=False,
+                               user_sfx=sfx)
+
         case 'allsnap':
             mcbkg = an.JPsiCCLoader('MC_BKG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
             mcbkg.createWeightedRDF('run_spec_mc_bkg_2018.json', 'event_spec_mc_bkg_2018.json')
@@ -45,8 +95,8 @@ if __name__=='__main__':
             mcbkg.snapshotRDF()
             mcbkg.makeCutFlowPlot()
             
-            databkg = an.JPsiCCLoader('DATA_BKG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
-            databkg.createDataRDF('event_spec_data_bkg_skim.json')
+            databkg = an.JPsiCCLoader('DATA', 2018, '202410', 'GF', 'CMSSW_13_3_0')
+            databkg.createDataRDF('event_spec_DATA_skim.json')
             databkg.defineColumnsRDF()
             databkg.snapshotRDF()
             databkg.makeCutFlowPlot()
@@ -59,7 +109,7 @@ if __name__=='__main__':
 
         case 'gen':
             mcsig = an.JPsiCCLoader('MC_SIG', 2018, '202410', 'GF', 'CMSSW_13_3_0')
-            mcsig.readSnapshot(f'snapshot_MC_SIG_2018_GF_v202410_{today_format}_WEIGHT_no_filter.root')
+            mcsig.readSnapshot(f'snapshot_MC_SIG_2018_GF_v202410_{today_format}_WEIGHT_{sfx}.root')
             mcsig.cut('nMuon<2')
             mcsig.cut('gen_muplus_pt>3.5')
             mcsig.cut('gen_muminus_pt>3.5')
@@ -74,12 +124,12 @@ if __name__=='__main__':
             mcsig = an.JPsiCCAnalyzer('MC_SIG', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
             mcbkg1 = an.JPsiCCAnalyzer('MC_BKG1', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
             mcbkg2 = an.JPsiCCAnalyzer('MC_BKG2', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
-            mcbkg1.readSnapshotSAMP('MC_BKG1', 'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root',
+            mcbkg1.readSnapshotSAMP('MC_BKG1', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_{today_format}_WEIGHT_{sfx}.root',
                                     sample_name='BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
-            mcbkg2.readSnapshotSAMP('MC_BKG2', 'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root',
+            mcbkg2.readSnapshotSAMP('MC_BKG2', f'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241119_{today_format}_{sfx}.root',
                                     sample_name='JpsiToMuMu_JpsiPt8_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2+MINIAODSIM')
-            # an.readSnapshotSAMP('DATA_BKG', 'snapshot_DATA_BKG_2018_GF_v202406_20240802_WEIGHT.root')
-            mcsig.readSnapshotSAMP('MC_SIG', 'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root')
+            # an.readSnapshotSAMP('DATA', 'snapshot_DATA_2018_GF_v202406_20240802_WEIGHT.root')
+            mcsig.readSnapshotSAMP('MC_SIG', f'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_20241119_{today_format}_{sfx}.root')
 
             if preset != 'cut': mcsig.stackMultiHistos(draw_indiv=draw_indiv)
             else:
@@ -105,15 +155,3 @@ if __name__=='__main__':
                 mcsig.makeCutTable(modify=False, plot=True)
                 mcsig.makeCutFlowPlotAll()
                 # mcsig.stackMultiHistos(draw_indiv=draw_indiv, user_sfx='cut')
-
-        case 'ancut':
-            mcsig = an.JPsiCCAnalyzer('MC_SIG', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
-            mcbkg1 = an.JPsiCCAnalyzer('MC_BKG1', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
-            mcbkg2 = an.JPsiCCAnalyzer('MC_BKG2', 2018, '202410', 'GF', 'CMSSW_13_3_0', weights=use_weight)
-            mcbkg1.readSnapshotSAMP('MC_BKG1', 'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root',
-                                    sample_name='BToJpsi_JPsiToMuMu_BMuonFilter_HardQCD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1+MINIAODSIM')
-            mcbkg2.readSnapshotSAMP('MC_BKG2', 'snapshot_MC_BKG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root',
-                                    sample_name='JpsiToMuMu_JpsiPt8_TuneCP5_13TeV-pythia8+RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2+MINIAODSIM')
-            # an.readSnapshotSAMP('DATA_BKG', 'snapshot_DATA_BKG_2018_GF_v202406_20240802_WEIGHT.root')
-            mcsig.readSnapshotSAMP('MC_SIG', 'snapshot_MC_SIG_2018_GF_v202410_CMSSW_13_3_0_20241119_WEIGHT_no_filter.root')
-            mcsig.addCutOptions('')
